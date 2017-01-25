5b51e9ea13713d725eb9e83b4d74bdfec017753b
WillBrennan <WillBrennan@users.noreply.github.com>
2017-01-25 23:53:40 +0000

    added movielens importer

--- recommender/data/movielens.py
+++ recommender/data/movielens.py
@@ -8,17 +8,14 @@ import pandas
 import numpy
 
 
-@typesrequired(str)
-def load_data(path):
+@typesrequired(str, str)
+def load_data(path, seperator):
     if not os.path.exists(path):
         raise ValueError("error! path does not exist")
 
-    if not os.path.basename(path) != "ratings.dat":
+    if not os.path.basename(path) != "ratings":
         raise ValueError("error! this only works on the ratings.dat file from movielens")
 
-    # todo(will) - finish this function
-
-    seperator = "\t"
     col_names = ["user", "item", "rate", "st"]
 
     datafile = pandas.read_csv(path, sep=seperator, header=None, names=col_names, engine='python')
--- tests/data/test_movielens.py
+++ tests/data/test_movielens.py
@@ -7,18 +7,29 @@ import pytest
 import numpy
 import pandas
 
-from recommender.cross_validation import *
+import recommender
+
+
+def test_data_exists():
+    path = "ratings.dat"
+    absolute_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
+
+    assert os.path.exists(absolute_path)
 
 
 def test_load_data():
     """
-    load ratings.csv and check againist hard coded parameters
-    ratings.csv is the first 100 lines of movielens-small
+    load ratings.dat and check againist hard coded parameters
+    ratings.dat is the first 100 lines of movielens-small
 
     :return:
     """
 
-    path = "ratings.csv"
+    path = "ratings.dat"
     absolute_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
 
     assert os.path.exists(absolute_path)
+
+    datafile = recommender.data.movielens.load_data(absolute_path, "::")
+
+    assert len(datafile) == 100
