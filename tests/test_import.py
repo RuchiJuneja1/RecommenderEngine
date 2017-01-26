355832269cc631b255abb44a7077d9b3ff045dd1
WillBrennan <WillBrennan@users.noreply.github.com>
2017-01-26 11:55:40 +0000

    added import tests

---
+++ tests/test_import.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+def test_imports():
+    import recommender
+    from recommender.cross_validation import *
+    from recommender import data
+    from recommender.data.movielens import *
