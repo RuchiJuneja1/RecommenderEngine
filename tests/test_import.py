02ae402aafbc80ca24457d2dbf425331e395f94e
WillBrennan <WillBrennan@users.noreply.github.com>
2017-01-26 14:48:45 +0000

    fixed imports for python3

--- tests/test_import.py
+++ tests/test_import.py
@@ -3,6 +3,6 @@ import pytest
 
 def test_imports():
     import recommender
-    from recommender.cross_validation import *
+    from recommender.cross_validation import split_dataset, split_dataframe
     from recommender import data
-    from recommender.data.movielens import *
+    from recommender.data.movielens import load_data
