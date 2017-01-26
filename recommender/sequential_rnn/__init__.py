f76be815c022e92a842b0c39eaef37ede37f206f
WillBrennan <WillBrennan@users.noreply.github.com>
2017-01-26 19:05:03 +0000

    added outline of sequential_rnn

---
+++ recommender/sequential_rnn/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import helpers
+
+__all__ = ["helpers"]
---
+++ tests/sequential_rnn/test_import.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pytest
+
+def test_import():
+    import recommender import sequential_rnn
