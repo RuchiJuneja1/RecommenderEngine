247d14a1b84d3021e0c7e74498f2bb08effd9461
WillBrennan <WillBrennan@users.noreply.github.com>
2017-01-26 00:12:46 +0000

    added better tests

--- tests/data/test_movielens.py
+++ tests/data/test_movielens.py
@@ -33,3 +33,14 @@ def test_load_data():
     datafile = recommender.data.movielens.load_data(absolute_path, "::")
 
     assert len(datafile) == 100
+
+    assert "user" in datafile.columns
+    assert 'item' in datafile.columns
+    assert 'rate' in datafile.columns
+    assert 'st' in datafile.columns
+
+    assert datafile.get_value(0, "user") == 0
+    assert datafile.get_value(0, "item") == 1192
+
+    assert datafile.get_value(10, "rate") == 5
+    assert datafile.get_value(20, "st") == 978302205
